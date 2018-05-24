0ca7a6a955a4f299e58c0033bb7eb4bf5afaf0db
hax <xuhaogmai50@gmail.com>
2018-05-24 20:29:32 +0200

    use tempfile.mkdtemp() to get a tmp directory
    why: the old way need to access home directory of user which is not possible in some cases, and use a static path
    have conflit in case several processes in the same time.

--- MyQR/myqr.py
+++ MyQR/myqr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import tempfile
 import os
 from MyQR.mylibs import theqrmodule
 from PIL import Image
@@ -85,7 +85,7 @@ def run(words, version=1, level='H', picture=None, colorized=False, contrast=1.0
         qr.resize((qr.size[0]*3, qr.size[1]*3)).save(qr_name)
         return qr_name
 
-    tempdir = os.path.join(os.path.expanduser('~'), '.myqr')
+    tempdir = tempfile.mkdtemp()
     
     try:
         if not os.path.exists(tempdir):
